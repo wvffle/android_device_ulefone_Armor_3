0c68d1b6cbf5b5cb72fe6e55281b9582f79d10e3
Kasper Seweryn <github@wvffle.net>
2021-10-08 09:37:16 +0200

    Initial commit

---
+++ omni_Armor_3.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Armor_3 device
+$(call inherit-product, device/ulefone/Armor_3/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Armor_3
+PRODUCT_NAME := omni_Armor_3
+PRODUCT_BRAND := ulefone
+PRODUCT_MODEL := Armor_3
+PRODUCT_MANUFACTURER := ulefone
